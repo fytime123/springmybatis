41e020fb384158c8ce81bd65cd79710dee796502
liufuyi <109335992@qq.com>
2019-08-03 20:36:03 +0800

    配置修改

--- src/main/java/com/example/demo/controller/demoController.java
+++ src/main/java/com/example/demo/controller/demoController.java
@@ -1,30 +1,27 @@
 package com.example.demo.controller;
 
-import com.example.demo.dao.userMapper;
 import com.example.demo.entity.user;
 import com.example.demo.service.demoControllerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class demoController {
 
     @Autowired
     private demoControllerService service;
 
-    @GetMapping("/springboot")
+    @RequestMapping("/hello")
     public String getName(@RequestParam("PK") String PK,  @RequestParam("password") String password){
 
         user resultUser = service.userLogin(PK, password);
         if (resultUser != null){
-            return "https://www.baidu.com";
+            return resultUser.getUsername();
 
         }else {
-            return "https://www.hao123.com";
+            return "没找到！";
         }
     }
 }
